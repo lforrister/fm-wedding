28b9cf7bd1f30f32f9b18c8fc1a67ffa49082f26
Lindsay Forrister <lindsayforrister@gmail.com>
2023-03-13 14:27:08 -0600

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import '../scss/components/Header.scss'
+
+const Header = () => {
+    return (
+        <>
+            <div className="header__bg">
+                <h1> I AM THE HEADER</h1>
+            </div>
+        
+        </>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
